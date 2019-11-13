e6884d08c0dbe48745fea43d47a8ccec59063cb2
djalmajr <djalmajr@gmail.com>
2019-11-13 12:10:16 -0300

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { mapActions, select } from 'unistore/src/util';
+
+export default store => (mapStateToProps, mapDispatchToProps) => {
+  if (typeof mapStateToProps !== 'function') {
+    mapStateToProps = select(mapStateToProps || {});
+  }
+
+  return Child => props => {
+    const state = mapStateToProps(store.getState(), props);
+    const actions = mapDispatchToProps
+      ? mapActions(mapDispatchToProps, store)
+      : { store };
+
+    return Child(Object.assign({}, actions, props, state));
+  };
+};
