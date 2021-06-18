f16046eb5dad42e744b7272b6107fb804903c139
djalmajr <djalmajr@gmail.com>
2021-06-17 21:30:58 -0300

    fix: handle null mapStateToProps

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { mapActions, select } from 'unistore/src/util';
 
-export default store => (mapStateToProps, mapDispatchToProps) => {
+const { assign, keys } = Object;
+
+export default (store) => (mapStateToProps, mapDispatchToProps) => {
   if (typeof mapStateToProps !== 'function') {
-    mapStateToProps = select(mapStateToProps || []);
+    mapStateToProps = select(mapStateToProps || keys(store.getState()));
   }
 
-  return Child => props => {
+  return (Child) => (props) => {
     const state = mapStateToProps(store.getState(), props);
     const actions = mapDispatchToProps
       ? mapActions(mapDispatchToProps, store)
       : { store };
 
-    return Child(Object.assign({}, actions, props, state));
+    return Child(assign({}, actions, props, state));
   };
 };
